e86462971b3d4dbde4fb9514088e4c9aafad030c
Lorenzo <caramaschilolo@gmail.com>
2022-09-22 22:51:06 -0300

    Correción Categorias con Firebase

--- src/components/itemListContainer/itemListContainer.jsx
+++ src/components/itemListContainer/itemListContainer.jsx
@@ -15,30 +15,45 @@ const ItemListContainer = () => {
 
     const obtenerProductos = async () => {
         setLoading(true)
+
+
         const db = getFirestore()
         const querySnapshot = await collection(db, 'items')
+
         getDocs(querySnapshot).then(res => {
-            const data = res.docs.map((doc) => {
-                return { id: doc.id, ...doc.data() }
-            })           
-            setListaProductos(data)
+            if (Object.keys(categoryName).length === 0) {
+                const data = res.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() }
+                })
+                setListaProductos(data)
+                console.log(listaProductos)
+            }
+            else {     
+                const data = res.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() }
+                })                                          
+                setListaProductos(data.filter((datos)=>datos.categoryId === categoryName.category))                                
+            }
+
             setLoading(false)
         })
+        
+        
     }
 
-    useEffect(() => {
-        obtenerProductos()
-    }, [categoryName])
+useEffect(() => {
+    obtenerProductos()
+}, [categoryName])
 
 
 
 
-    return (
-        <NavLink to={'/'} style={{ textDecoration: 'none', color: '#42506b' }}>
-            {loading ? (<div className="lds-ring"><div></div><div></div><div></div><div></div></div>) : <ItemList lista={listaProductos} />}
-        </NavLink>
+return (
+    <NavLink to={'/'} style={{ textDecoration: 'none', color: '#42506b' }}>
+        {loading ? (<div className="lds-ring"><div></div><div></div><div></div><div></div></div>) : <ItemList lista={listaProductos} />}
+    </NavLink>
 
-    )
+)
 }
 
 export default ItemListContainer;
