c75b81a12cbab73489b8837ddd4ce2eb7f73a635
Lorenzo <caramaschilolo@gmail.com>
2022-09-29 19:39:41 -0300

    Desafio: Item Collection

--- src/components/itemDetailContainer/itemDetailContainer.jsx
+++ src/components/itemDetailContainer/itemDetailContainer.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "../itemDetail/itemDetail";
-import { data } from "../mockData";
 import './item-detail-container.css';
-import { getFirestore, getDocs, collection } from "firebase/firestore"
+import { getFirestore, getDocs, collection, where, query } from "firebase/firestore"
 
 
 const ItemDetailContainer = () => {
@@ -14,16 +13,32 @@ const ItemDetailContainer = () => {
     const obtenerProducto = async () => {
         const db = getFirestore()
         const querySnapshot = await collection(db, 'items')
+
+        if(id){
+            const queryFilter = query(
+                querySnapshot,
+                where("categoryId","==",id)
+            )
+            getDocs(queryFilter).then(res => {
+                const data = res.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() }
+                })
+                const dataFilter = data.filter((doc) => doc.title === id.titulo)
+                setProduct(...dataFilter)
+            })
+                .catch((err) => {
+                    console.log(err)                
+                })
+        
+        }
         getDocs(querySnapshot).then(res => {
             const data = res.docs.map((doc) => {
                 return { id: doc.id, ...doc.data() }
-            })
-            const dataFilter = data.filter((doc) => doc.title === id.titulo)
-            setProduct(...dataFilter)
+            })            
+            setProduct(...data)
         })
             .catch((err) => {
-                console.log(err)
-                console.log("hey");
+                console.log(err)                
             })
     }
 
